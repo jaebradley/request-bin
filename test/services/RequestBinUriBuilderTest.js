33dbe795a9c776e5f72040961c19f4fca01b9208
jaebradley <jae.b.bradley@gmail.com>
2017-07-10 17:40:30 -0400

    add test cases

--- test/services/RequestBinUriBuilderTest.js
+++ test/services/RequestBinUriBuilderTest.js
@@ -9,4 +9,32 @@ describe('Test Request Bin Uri Builder', () => {
       RequestBinUriBuilder.getHost().should.equal('https://requestb.in/api/v1');
     });
   });
+
+  describe('#getCreateBinUri', () => {
+    it('should return the URI to create a bin', () => {
+      RequestBinUriBuilder.getCreateBinUri().should.equal('https://requestb.in/api/v1/bins');
+    });
+  });
+
+  describe('#getBinUri', () => {
+    it('should return the URI to fetch a specific bin', () => {
+      const binId = 'binId';
+      RequestBinUriBuilder.getBinUri(binId).should.equal('https://requestb.in/api/v1/bins/binId');
+    });
+  });
+
+  describe('#getBinRequestsUri', () => {
+    it('should return the URI to fetch requests for a specific bin', () => {
+      const binId = 'binId';
+      RequestBinUriBuilder.getBinRequestsUri(binId).should.equal('https://requestb.in/api/v1/bins/binId/requests');
+    });
+  });
+
+  describe('#getBinRequestUri', () => {
+    it('should return the URI to fetch a specific request for a specific bin', () => {
+      const binId = 'binId';
+      const requestId = 'requestId';
+      RequestBinUriBuilder.getBinRequestUri(binId, requestId).should.equal('https://requestb.in/api/v1/bins/binId/requests/requestId');
+    });
+  });
 });
